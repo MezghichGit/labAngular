23502f7bae7c47286fe9f68ef354d1bae97cfe85
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-02-16 14:38:24 +0100

    creation de service & injection de dependances

--- src/app/stagiaires/stagiaires.component.ts
+++ src/app/stagiaires/stagiaires.component.ts
@@ -1,27 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MypipePipe } from "../mypipe.pipe";
+import { StagiaireService } from '../services/stagiaire.service';
+
 
 @Component({
-    selector: 'app-stagiaires',
-    standalone: true,
-    templateUrl: './stagiaires.component.html',
-    styleUrl: './stagiaires.component.css',
-    imports: [CommonModule, MypipePipe]
+  selector: 'app-stagiaires',
+  standalone: true,
+  templateUrl: './stagiaires.component.html',
+  styleUrl: './stagiaires.component.css',
+  imports: [CommonModule, MypipePipe]
 })
-export class StagiairesComponent {
+export class StagiairesComponent implements OnInit {
 
-  formateur:string="Mohamed Amine MEZGHICH";
-  logo:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhfJQZMxfTEgkCwULqbvcifmRV9CVpEjufFsf9xvKoQ&s";
+  formateur: string = "Mohamed Amine MEZGHICH";
+  logo: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhfJQZMxfTEgkCwULqbvcifmRV9CVpEjufFsf9xvKoQ&s";
 
-  afficherMsg:boolean = true;
-  greeting()
-  {
-    //alert("Bonjour mes chers étudiants!");
-    this.afficherMsg = !this.afficherMsg;
+  afficherMsg: boolean = true;
+
+  // injection d'une dépendance : Sevice Stagiaire
+  constructor(private serviceStagiaire: StagiaireService) {
+    console.log("Constructeur");
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit");
   }
 
-  bye(){
+
+  greeting() {
+    //alert("Bonjour mes chers étudiants!");
+   // this.afficherMsg = !this.afficherMsg;
+   let msg : string = this.serviceStagiaire.getData();
+   alert(msg);
+  }
+
+  bye() {
     alert("A très Bientôt!");
   }
 }
