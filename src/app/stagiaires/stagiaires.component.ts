7395a3f95bf6cae6596ec5128587e5288dfed3cf
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-02-16 14:41:09 +0100

    avec ngOnInit

--- src/app/stagiaires/stagiaires.component.ts
+++ src/app/stagiaires/stagiaires.component.ts
@@ -17,21 +17,22 @@ export class StagiairesComponent implements OnInit {
   logo: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhfJQZMxfTEgkCwULqbvcifmRV9CVpEjufFsf9xvKoQ&s";
 
   afficherMsg: boolean = true;
-
+  msg:string="";
   // injection d'une dépendance : Sevice Stagiaire
   constructor(private serviceStagiaire: StagiaireService) {
     console.log("Constructeur");
   }
   ngOnInit(): void {
     console.log("ngOnInit");
+    this.msg = this.serviceStagiaire.getData();  //prepare les data
   }
 
 
   greeting() {
     //alert("Bonjour mes chers étudiants!");
    // this.afficherMsg = !this.afficherMsg;
-   let msg : string = this.serviceStagiaire.getData();
-   alert(msg);
+
+   alert(this.msg);
   }
 
   bye() {
