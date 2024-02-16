b9dbe57db25f6e6d89c4a2c168f8557ffbf48ee5
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-02-16 12:31:51 +0100

    pipe & directire ngIf

--- src/app/stagiaires/stagiaires.component.ts
+++ src/app/stagiaires/stagiaires.component.ts
@@ -1,20 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MypipePipe } from "../mypipe.pipe";
 
 @Component({
-  selector: 'app-stagiaires',
-  standalone: true,
-  imports: [],
-  templateUrl: './stagiaires.component.html',
-  styleUrl: './stagiaires.component.css'
+    selector: 'app-stagiaires',
+    standalone: true,
+    templateUrl: './stagiaires.component.html',
+    styleUrl: './stagiaires.component.css',
+    imports: [CommonModule, MypipePipe]
 })
 export class StagiairesComponent {
 
   formateur:string="Mohamed Amine MEZGHICH";
   logo:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhfJQZMxfTEgkCwULqbvcifmRV9CVpEjufFsf9xvKoQ&s";
 
+  afficherMsg:boolean = true;
   greeting()
   {
-    alert("Bonjour mes chers étudiants!");
+    //alert("Bonjour mes chers étudiants!");
+    this.afficherMsg = !this.afficherMsg;
   }
 
   bye(){
