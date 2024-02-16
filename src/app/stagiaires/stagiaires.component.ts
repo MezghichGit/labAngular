08691c94738398e1829dfbb6b46297ebe3fffe0f
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-02-16 10:58:37 +0100

    string interpolation - property binding - event binding

--- src/app/stagiaires/stagiaires.component.ts
+++ src/app/stagiaires/stagiaires.component.ts
@@ -10,10 +10,14 @@ import { Component } from '@angular/core';
 export class StagiairesComponent {
 
   formateur:string="Mohamed Amine MEZGHICH";
+  logo:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyhfJQZMxfTEgkCwULqbvcifmRV9CVpEjufFsf9xvKoQ&s";
 
   greeting()
   {
     alert("Bonjour mes chers étudiants!");
   }
 
+  bye(){
+    alert("A très Bientôt!");
+  }
 }
